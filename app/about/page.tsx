732be851f929721cb1781a78d0706ea9cf460590
Rohan-Shakya <rohanshakya254@gmail.com>
2023-03-08 17:23:48 +0545

    done about and blog page

---
+++ app/about/page.tsx
@@ -0,0 +1,16 @@
+import About from "@/components/About";
+import Breadcrumb from "@/components/Common/Breadcrumb";
+
+const AboutPage = () => {
+  return (
+    <>
+      <Breadcrumb
+        pageName="About Page"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
+      />
+      <About />
+    </>
+  );
+};
+
+export default AboutPage;
